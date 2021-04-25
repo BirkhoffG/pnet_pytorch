43f0818b9b0a1d4b3de275c48372bcaa9635974c
ethan5437 <32521027+ethan5437@users.noreply.github.com>
2021-04-26 00:53:17 +0800

    add adversarial and batch

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from .tp_data_reader import get_dataset
 from .vocabulary import Vocabulary
 from .example import Example
 from .models.attacker import *
-from .dataset import PrDataset
+from .dataset import PrDataset, AttackDataset
 
 from collections import defaultdict
 import torch.nn as nn
@@ -41,6 +41,7 @@ def get_aux_labels(examples):
 class PrModel:
     def __init__(self, args, vocabulary: Vocabulary, classifier_output_size: int, adversary_output_size: int) -> None:
         self.args = args
+        self.device = args.device
 
         self.vocabulary = vocabulary
 
@@ -48,55 +49,64 @@ class PrModel:
         self.main_classifier = MainClassifier(
             alphabet_size=vocabulary.size_chars(), vocab_size=vocabulary.size_words(), 
             output_size=classifier_output_size, args=args
-            )
+            ).to(self.device)
         self.adversary_classifier = AdversaryClassifier(
             self.main_classifier.hidden_size, 
             output_size=adversary_output_size, args=args
-            )
+            ).to(self.device)
     
     def get_input(self, example: Example, adversarial=False):
         return self.vocabulary.code_sentence_cw(example.get_sentence(), adversarial=adversarial)
 
     def evaluate_main(self, dataset):
         self.main_classifier.eval()
-
+        device = self.device
+        
         loss = 0
         acc = 0
-        tot = len(dataset)
-        data_loader = DataLoader(dataset, batch_size=args.batch_size)
-
-        for i, (input_vec, target) in enumerate(data_loader):
-            l, p = self.main_classifier.get_loss_prediction(input_vec, target)
-            loss += l.item()
-            acc += sum(p == target).item()
-        return loss / tot, acc / tot * 100
+        tot = 0#len(dataset)
+        with torch.no_grad():
+            for i, (input_vec, target) in enumerate(dataset):
+                input_vec = input_vec.to(device)
+                target = target.to(device)
+                l, predicts = self.main_classifier.get_loss_prediction(input_vec, target)
+                loss += l.item()
+                for p, t in zip(predicts, target):
+                    tot += 1
+                    if p.item() == t.item():
+                        acc += 1
+#                 print(acc, tot)
+        return (loss / tot), round(acc / tot  * 100, 3)
 
 
     def train_main(self, train, dev):
         lr = self.args.learning_rate
         batch_size = self.args.batch_size
-
-        train_dataset = PrDataset(train, self.vocabulary, args)
-        val_dataset = PrDataset(dev, self.vocabulary, args)
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        device = self.device
+        
+        train_dataset = PrDataset(train, self.vocabulary, self.args.seq_len)
+        val_dataset = PrDataset(dev, self.vocabulary, self.args.seq_len)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
         optimizer = optim.AdamW(self.main_classifier.parameters(), lr=lr)
-
+        
         # epoch 0
-        l, acc = self.evaluate_main(val_dataset)
+        l, acc = self.evaluate_main(val_loader)
         print(f"[epoch=0] loss: {l}, acc: {acc}%")
 
-        for i in range(self.args.iterations):    
-
-            # train_dataset.shuffle()
+        for i in range(self.args.iterations):
             self.main_classifier.train()
-
             for _i, (input_vec, target) in enumerate(tqdm(train_loader)):
+#                 print('input_vec',input_vec.shape)
                 # sys.stderr.write("\r{}%".format(_i / len(train_dataset) * 100))
-                
+                input_vec = input_vec.to(device)
+                target = target.to(device)
                 loss = self.main_classifier.get_loss(input_vec, target)
                 loss.backward()
-
+                # mimic batchingc
+#                 if (_i + 1) % batch_size == 0:
                 optimizer.step()
                 optimizer.zero_grad()
 
@@ -108,13 +118,73 @@ class PrModel:
                 
                 # if self.args.generator:
                 #     generator_loss += self.generator_train(example)
+            l, acc = self.evaluate_main(val_loader)
+            print(f"[epoch={i+1}] loss: {l}, acc: {acc}")
             
-            l, acc = self.evaluate_main(val_dataset)
-            print(f"[epoch={i+1}] loss: {l}, acc: {acc}%")
-            
-        
+ 
+    def evaluate_adversarial(self, dataset):
+        self.adversary_classifier.eval()
+        self.main_classifier.eval()
+        device = self.device
+        loss = 0
+        gender_acc = 0
+        age_acc = 0
+        tot = 0#len(dataset)
+        with torch.no_grad():
+            for i, (input_vec, target) in enumerate(dataset):
+                input_vec = input_vec.to(device)
+                target = target.to(device)
+                hidden_state = self.main_classifier.get_lstm_embed(input_vec)
+                l, predicts = self.adversary_classifier.get_loss_prediction(hidden_state, target)
+                loss += l.item()
+                for p, t in zip(predicts, target):
+                    tot += 1
+                    if p[0].item() == t[0].item():
+                        gender_acc += 1
+                    if p[1].item() == t[1].item():
+                        age_acc += 1
+        return loss / tot, round(gender_acc / tot * 100, 3) , round(age_acc / tot * 100, 3) 
+
     def train_adversarial(self, train, dev):
-        pass
+        lr = self.args.learning_rate
+        batch_size = self.args.batch_size
+        device = self.device
+        output_size =  self.adversary_classifier.output_size
+        seq_len = self.args.seq_len
+        
+        train_dataset = AttackDataset(train, self.vocabulary, seq_len, output_size)
+        val_dataset = AttackDataset(dev, self.vocabulary, seq_len, output_size)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+        
+        optimizer = optim.AdamW(self.adversary_classifier.parameters(), lr=lr)
+
+        # epoch 0
+        l, gender_acc, age_acc = self.evaluate_adversarial(val_loader)
+        print(f"[epoch=0] loss: {l}, gender acc: {gender_acc}%, gender acc: {age_acc}%")
+
+        for i in range(self.args.iterations):
+            self.adversary_classifier.train()
+            self.main_classifier.train()
+            for _i, (input_vec, target) in enumerate(tqdm(train_loader)):
+                input_vec = input_vec.to(device)
+                target = target.to(device)
+                hidden_state = self.main_classifier.get_lstm_embed(input_vec)
+                loss = self.adversary_classifier.get_loss(hidden_state, target)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+                # if self.args.ptraining:
+                #     self.privacy_train(example, train)
+                
+                # if self.args.atraining:
+                #     discriminator_loss += self.discriminator_train(example)
+                
+                # if self.args.generator:
+                #     generator_loss += self.generator_train(example)
+            l, gender_acc, age_acc = self.evaluate_adversarial(val_loader)
+            print(f"[epoch={i+1}] loss: {l}, gender acc: {gender_acc}%, gender acc: {age_acc}%")
 
     def evaluate_influence_sample(self, train):
         pass
@@ -122,6 +192,8 @@ class PrModel:
 
 def main(args):
 
+    device = torch.device(f'cuda:{args.device}' if args.device != 'cpu' else 'cpu')
+    args.device = device
     torch.manual_seed(0)
     get_data = {"tp_fr": lambda : get_dataset("fr"),
                 "tp_de": lambda : get_dataset("de"),
@@ -144,6 +216,8 @@ def main(args):
     mod = PrModel(args, vocabulary, classifier_output_size, adversary_output_size)
     
     mod.train_main(train, dev)
+    mod.train_adversarial(train, dev)
+    
 
 
 if __name__ == "__main__":
@@ -153,7 +227,7 @@ if __name__ == "__main__":
     import os
     random.seed(10)
     np.random.seed(10)
-    torch.manual_seed(10)
+    torch.manual_seed(0)
     
     usage = """Implements the privacy evaluation protocol described in the article.
 
@@ -168,10 +242,11 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description = usage, formatter_class=argparse.RawTextHelpFormatter)
     
-    parser.add_argument("--learning-rate", "-lr", type=float, default=0.1)
-    parser.add_argument("--batch-size", "-b", type=int, default=256)
-    parser.add_argument("--iterations", "-i", type=int, default=20, help="Number of training iterations")
+    parser.add_argument("--learning-rate", "-b", type=float, default=1e-4)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--iterations", "-i", type=int, default=5, help="Number of training iterations")
     
+    parser.add_argument("--seq_len", "-sl", type=int, default=75, help="Length of sequence")
 
     # define model parameters
     parser.add_argument("--char-embed-dim","-c", type=int, default=50, help="Dimension of char embeddings")
@@ -180,8 +255,8 @@ if __name__ == "__main__":
     parser.add_argument("--word-hidden-dim","-W", type=int, default=50, help="Dimension of word lstm")
 
     parser.add_argument("--fc-dim","-l", type=int, default=50, help="Dimension of hidden layers")
-
-    parser.add_argument("--seq_len", default=80, help="Sequence length")
+    
+    parser.add_argument("--device", "-d", type=str, default='cpu', help="Length of sequence")
     
     args = parser.parse_args()
 
